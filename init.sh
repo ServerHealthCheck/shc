7f5b70429266ba6cfb97b95f470d49dd86697f51
Johannes Unglert <unglert@b1-systems.de>
2020-04-15 12:48:42 +0200

    2nd Commit: Corrected errors

--- init.sh
+++ init.sh
@@ -8,31 +8,31 @@
 #variables
 user="$USER"
 site_name="index.php"
-script_name="shc_scripts"
-script_path="/home/$user/$script_name"
+folder_name="shc_scripts"
+folder_path="/home/$user/$script_name"
 splitter="----------------------------------------------------------------"
 
 #functions
 
 #generate folder for the scripts, if not already generated
 echo "
-Initial check if folder $script_name exists
+Initial check if folder $folder_name exists
 "
-if [ -d "$ansible_path" ];
+if [ -d "$folder_path" ];
     then
             echo -e "Folder already exists \e[32m\u2714 \e[39m
             "
             echo $splitter
     else
-            echo "Creating $script_name folder in /home/$user/
+            echo "Creating $folder_name folder in /home/$user/
             "
-            mkdir /home/$user/$script_name
+            mkdir /home/$user/$folder_name
             echo $splitter
 fi
 
 #move scripts into folder
 echo "
-Moving scripts into $script_path
+Moving scripts into $folder_path
 "
 mv *.sh $script_path
 
@@ -40,6 +40,6 @@ mv *.sh $script_path
 echo "
 Moving $site_name into apache2 default place (needs sudo)
 "
-sudo mv $site_name /srv/www/htdocs/
+sudo mv $site_name.php /srv/www/htdocs/
 
  
