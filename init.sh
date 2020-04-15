f28195128122005d99903b2f17b3458ebaf06f21
Johannes Unglert <unglert@b1-systems.de>
2020-04-15 13:15:34 +0200

    4th Commit: Changed language

--- init.sh
+++ init.sh
@@ -10,10 +10,8 @@ user="$USER"
 site_name="index.php"
 folder_name="shc_scripts"
 folder_path="/home/$user/$script_name"
-splitter="----------------------------------------------------------------"
 
 #functions
-
 #generate folder for the scripts, if not already generated
 echo "
 Initial check if folder $folder_name exists
@@ -22,12 +20,10 @@ if [ -d "$folder_path" ];
     then
             echo -e "Folder already exists \e[32m\u2714 \e[39m
             "
-            echo $splitter
     else
             echo "Creating $folder_name folder in /home/$user/
             "
             mkdir /home/$user/$folder_name
-            echo $splitter
 fi
 
 #move scripts into folder
@@ -42,4 +38,6 @@ Moving $site_name into apache2 default place (needs sudo)
 "
 sudo mv $site_name /srv/www/htdocs/
 
- 
+echo "
+Now you can access the apachesite by typing http://localhost
+For any help consult the Readme"
