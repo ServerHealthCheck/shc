71ce718eb0d6fd69b2637b09beeeab91d81e7919
Johannes <47390613+junglert@users.noreply.github.com>
2020-09-08 16:43:47 +0200

    Update storage_percentage.sh

--- shc_scripts/storage_percentage.sh
+++ shc_scripts/storage_percentage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 storage_used=$(bash $HOME/shc_scripts/storage_used.sh)
 storage_total=$(bash $HOME/shc_scripts/storage_total.sh)
+
 echo $(( (storage_used*100)/storage_total ))                                            
