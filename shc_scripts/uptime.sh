4b653614b793a4de490d424df561450a0bc71511
DoctorCr4nk <malex@kabelmail.de>
2020-09-21 20:32:24 +0200

    fixed raspbian block

--- shc_scripts/uptime.sh
+++ shc_scripts/uptime.sh
@@ -5,9 +5,9 @@ OS_flavour=$(grep "ID_LIKE" /etc/os-release | cut -d'=' -f2 | sed 's/"//g')
 # get uptime (removed extra foo)
 uptime=$(uptime | awk -F "up" '{print $2}' | cut -d, -f1 | sed 's/^ *//g')
 
-if $(grep -i raspbian /etc/os-release)
+if [ $(grep -i raspbian /etc/os-release ; echo $?) -eq 1 ]
 then
-    echo "n/a"
-else
     echo ${uptime}
+else
+    echo "n/a"
 fi
