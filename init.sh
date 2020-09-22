773f8121189a98ccdede9507b5ab5d0d82179489
Johannes <47390613+junglert@users.noreply.github.com>
2020-09-22 16:25:39 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -46,6 +46,8 @@ case "$package" in
 			echo "Starting apache2.service and php7 module"
 			sudo systemctl enable --now apache2.service
 			sudo a2enmod php7
+			echo "
+			"
 		else
 			echo "apache2 and php7 module are already installed"
 			echo "Adding index.php to /etc/apache2/httpd.conf"
@@ -53,6 +55,8 @@ case "$package" in
 			echo "Starting apache2.service and php7 module"
 			sudo systemctl enable --now apache2.service
 			sudo a2enmod php7
+			echo "
+			"
 		fi	
 	;;
 
