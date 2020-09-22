9494fb2e600f52c134b36ff25dcccfb0dde666e3
Johannes <47390613+junglert@users.noreply.github.com>
2020-09-22 16:51:47 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -71,7 +71,7 @@ case "$package" in
 		sudo apt-get install -y apache2 libapache2-mod-php
 		echo "Starting apache2.service and php7.4 module"
 		sudo systemctl enable --now apache2.service
-		sudo a2enmod php7.4
+		sudo a2enmod php7.3
 	;;
 
 	*)
