00c795d88b5a1ef936d8b14c7c8749f54bfe933e
Johannes <47390613+junglert@users.noreply.github.com>
2020-09-22 17:58:19 +0200

    Update temperature.sh

--- shc_scripts/temperature.sh
+++ shc_scripts/temperature.sh
@@ -4,8 +4,6 @@ cores=$(cat /sys/class/thermal/thermal_zone*/temp)
 for i in $cores
 	do core_new=$(($core_new+$i))
  	   counter=$(($counter+1))
-		echo $counter
-		echo $core_new
 	done
 
 echo $(($core_new/$counter/1000))
