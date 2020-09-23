a00c920a68c1393aabe7a75804f13851a42a3888
DoctorCr4nk <malex@kabelmail.de>
2020-09-23 14:43:08 +0200

    removed unused code

--- shc_scripts/uptime.sh
+++ shc_scripts/uptime.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# get the distro flavour
-OS_flavour=$(grep "ID_LIKE" /etc/os-release | cut -d'=' -f2 | sed 's/"//g')
 # get uptime (removed extra foo)
 uptime=$(uptime | awk -F "up" '{print $2}' | cut -d, -f1 | sed 's/^ *//g')
-
-if [ $(grep -i raspbian /etc/os-release ; echo $?) -eq 1 ]
-then
-    echo ${uptime}
-else
-    echo "n/a"
-fi
